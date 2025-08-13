af7badf032b97379a5489a149421dde7483396e8
Yegor Gera <g3eraa@gmail.com>
2025-08-13 16:56:54 +0300

    Simple pr v2

---
+++ src/cc++project/Sample4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int length, width;
+    cout << "Enter the length: ";
+    cin >> length;
+    cout << "Enter the width: ";
+    cin >> width;
+    cout << "Area is " << length*width;
+    return 0;
+}
